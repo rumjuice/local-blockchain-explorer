90e51fa7a54fe9664d010c091cb4a2dc27b35a7e
rumjuice <ramdhani@live.com>
2022-04-24 23:52:04 -0400

    fix header

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,27 +1,50 @@
+import {Route, RouteObject, Routes} from 'react-router-dom';
 import logo from './logo.svg';
 import Header from './modules/Header';
 
 function App() {
+  const routes: RouteObject[] = [
+    {
+      path: '/transaction',
+      // element: <Login />,
+    },
+  ];
+
   return (
     <div className="flex justify-center items-center h-screen w-screen">
-      <div className="container-lg w-3/5 max-w-lg rounded-lg border-2 shadow">
+      <div className="absolute top-5 text-xl italic font-bold text-sky-900">
+        {' '}
+        Blockchain Explorer
+      </div>
+      <div className="container-lg w-3/5 max-w-lg rounded-xl border-2 shadow">
         {/* header */}
         <Header />
         {/* body */}
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <input disabled={false} />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer">
-            Learn React
-          </a>
-        </header>
+        <div className="p-4">
+          <Routes>
+            {routes.map((route) => (
+              <Route
+                key={route.path}
+                path={route.path}
+                element={route.element}
+              />
+            ))}
+          </Routes>
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <input disabled={false} />
+            <p>
+              Edit <code>src/App.tsx</code> and save to reload.
+            </p>
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer">
+              Learn React
+            </a>
+          </header>
+        </div>
       </div>
     </div>
   );
--- apps/web/src/modules/Header/Header.tsx
+++ apps/web/src/modules/Header/Header.tsx
@@ -1,11 +1,13 @@
 import {Tab} from '@headlessui/react';
 import {FC} from 'react';
+import {useNavigate} from 'react-router-dom';
 
 function classNames(...classes: string[]) {
   return classes.filter(Boolean).join(' ');
 }
 
 const Header: FC = () => {
+  // TODO put this on each module
   const tabs = [
     {
       id: 1,
@@ -24,24 +26,26 @@ const Header: FC = () => {
     },
   ];
 
+  const navigate = useNavigate();
+
   const handleTabs = (i: number) => {
-    console.log('clicked', i);
+    navigate(tabs[i].route);
   };
 
   return (
-    <div className="w-full max-w-md px-2 py-16 sm:px-0">
+    <div className="w-full mb-4">
       <Tab.Group onChange={handleTabs}>
-        <Tab.List className="flex p-1 space-x-1 bg-blue-900/20 rounded-xl">
+        <Tab.List className="flex p-1 space-x-1 bg-sky-600 rounded-xl">
           {tabs.map((tab) => (
             <Tab
               key={tab.id}
               className={({selected}) =>
                 classNames(
-                  'w-full py-2.5 text-sm leading-5 font-medium text-blue-700 rounded-lg',
-                  'focus:outline-none focus:ring-2 ring-offset-2 ring-offset-blue-400 ring-white ring-opacity-60',
+                  'w-full py-2.5 text-sm leading-5 font-medium text-sky-700 rounded-lg',
+                  'focus:outline-none focus:ring-2 ring-offset-2 ring-offset-sky-400 ring-white ring-opacity-60',
                   selected
                     ? 'bg-white shadow'
-                    : 'text-blue-100 hover:bg-white/[0.12] hover:text-white',
+                    : ' text-sky-100 hover:bg-white/[0.12] hover:text-white',
                 )
               }>
               {tab.title}
